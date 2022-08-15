fff7466270b011271bb1adde6978c28f0c957ac1
Ikaros <327209194@qq.com>
2022-08-15 10:41:16 +0800

    新增查b站用户信息功能

---
+++ src/plugins/search_bilibili_info.py
@@ -0,0 +1,47 @@
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+import nonebot
+import requests
+
+catch_str = on_keyword({'/查b '})
+
+
+@catch_str.handle()
+async def send_msg(bot: Bot, event: Event, state: T_State):
+    get_msg = str(event.get_message())
+    # nonebot.logger.info(get_msg)
+    content = get_msg[4:]
+
+    base_info_json = await get_base_info(content)
+    room_id = await get_room_id(content)
+    guard_info_json = await get_guard_info(content, room_id)
+
+    id = event.get_user_id()
+    msg = "[CQ:at,qq={}]".format(id) + '\n用户名：' + base_info_json['card']['name'] + '\nUID：' + str(base_info_json['card']['mid']) + \
+          '\n房间号：' + str(room_id) + '\n舰团数：' + str(guard_info_json['data']['info']['num'])
+    await catch_str.finish(Message(f'{msg}'))
+
+
+async def get_base_info(uid):
+    API_URL = 'https://account.bilibili.com/api/member/getCardByMid?mid=' + uid
+    ret = requests.get(API_URL)
+    ret = ret.json()
+    # nonebot.logger.info(ret)
+    return ret
+
+
+async def get_room_id(uid):
+    API_URL = 'https://api.live.bilibili.com/room/v2/Room/room_id_by_uid?uid=' + uid
+    ret = requests.get(API_URL)
+    ret = ret.json()
+    room_id = ret['data']['room_id']
+    return room_id
+
+
+async def get_guard_info(uid, room_id):
+    API_URL = 'https://api.live.bilibili.com/xlive/app-room/v2/guardTab/topList?roomid=' + str(room_id) + '&page=1&ruid=' + uid + '&page_size=0'
+    ret = requests.get(API_URL)
+    ret = ret.json()
+    return ret
