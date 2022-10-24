393ba7b559f2671b438d2bf972c7a1c5d8294072
Ikaros <327209194@qq.com>
2022-10-24 21:21:32 +0800

    新增查询历史上今天的事件

---
+++ src/plugins/history_today.py
@@ -0,0 +1,25 @@
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+import requests
+
+catch_str = on_keyword({'/历史上今天'})
+
+
+@catch_str.handle()
+async def send_msg(bot: Bot, event: Event, state: T_State):
+    id = event.get_user_id()
+
+    data = await get_data()
+    msg = "[CQ:at,qq={}]".format(id) + '\n' + data
+
+    await catch_str.finish(Message(f'{msg}'))
+
+
+async def get_data():
+    API_URL = 'https://api.linhun.vip/api/history?format=json'
+    ret = requests.get(API_URL)
+    ret = ret.text
+    # nonebot.logger.info(ret)
+    return ret
