67bb80d0fed3bbde1d14345e86577f88b71394b4
Ikaros <327209194@qq.com>
2022-10-24 21:43:44 +0800

    新增随机段子功能

---
+++ src/plugins/search_QCellCore.py
@@ -0,0 +1,27 @@
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+import requests
+
+catch_str = on_keyword({'/查手机号 '})
+
+
+@catch_str.handle()
+async def send_msg(bot: Bot, event: Event, state: T_State):
+    id = event.get_user_id()
+    get_msg = str(event.get_message())
+    content = get_msg[6:]
+
+    data = await get_data(content)
+    msg = "[CQ:at,qq={}]".format(id) + '\n' + data
+
+    await catch_str.finish(Message(f'{msg}'))
+
+
+async def get_data(content):
+    API_URL = 'https://api.linhun.vip/api/phone?tel=' + content
+    ret = requests.get(API_URL)
+    ret = ret.text
+    # nonebot.logger.info(ret)
+    return ret
