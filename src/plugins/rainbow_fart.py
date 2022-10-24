9941e0365700c3bf8ba326fdabb5ef5865174b5b
Ikaros <327209194@qq.com>
2022-10-24 23:38:57 +0800

    新增彩虹屁；舔狗日记；疯狂星期四

---
+++ src/plugins/rainbow_fart.py
@@ -0,0 +1,25 @@
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+import requests
+
+catch_str = on_keyword({'/彩虹屁'})
+
+
+@catch_str.handle()
+async def send_msg(bot: Bot, event: Event, state: T_State):
+    id = event.get_user_id()
+
+    data = await get_data()
+    msg = "[CQ:at,qq={}]".format(id) + '\n' + data['data']['text']
+
+    await catch_str.finish(Message(f'{msg}'))
+
+
+async def get_data():
+    API_URL = 'https://api.shadiao.pro/chp'
+    ret = requests.get(API_URL)
+    ret = ret.json()
+    # nonebot.logger.info(ret)
+    return ret
