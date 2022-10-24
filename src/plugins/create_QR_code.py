924804b95710fbbac2855addf5d117f6a1128e06
Ikaros <327209194@qq.com>
2022-10-24 20:53:11 +0800

    新增合成二维码功能

---
+++ src/plugins/create_QR_code.py
@@ -0,0 +1,17 @@
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+import requests
+
+catch_str = on_keyword({'/合成二维码 '})
+
+
+@catch_str.handle()
+async def send_msg(bot: Bot, event: Event, state: T_State):
+    id = event.get_user_id()
+    get_msg = str(event.get_message())
+    content = get_msg[7:]
+
+    msg = "[CQ:image,file=https://api.linhun.vip/api/QRcode?url=" + content + "]"
+    await catch_str.finish(Message(f'{msg}'))
