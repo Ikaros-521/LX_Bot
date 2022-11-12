8586655df5ecd8103f77dda8586e2f8a2fddbee3
Ikaros <327209194@qq.com>
2022-11-12 11:52:23 +0800

    b

--- src/plugins/bili_video_schedule.py
+++ src/plugins/bili_video_schedule.py
@@ -8,13 +8,12 @@ import requests
 
 catch_str = on_keyword({'今日番剧'})
 
-
 @catch_str.handle()
-async def send_img(bot: Bot, event: Event, state: T_State):
+async def send_msg(bot: Bot, event: Event, state: T_State):
     # get_msg = str(event.get_message())
     # nonebot.logger.info(get_msg)
     id = event.get_user_id()
-    json1 = await get_info()
+    json1 = await get_info(1, 0, 0)
     if json1["code"] != 0:
         msg = "[CQ:at,qq={}]".format(id) + "\n接口获取数据异常，请联系管理员修复喵~"
         await catch_str.finish(Message(f'{msg}'))
@@ -32,9 +31,59 @@ async def send_img(bot: Bot, event: Event, state: T_State):
         await catch_str.finish(Message(f'{msg}'))
 
 
-async def get_info():
+catch_str2 = on_keyword({'昨日番剧'})
+
+@catch_str2.handle()
+async def send_msg2(bot: Bot, event: Event, state: T_State):
+    # get_msg = str(event.get_message())
+    # nonebot.logger.info(get_msg)
+    id = event.get_user_id()
+    json1 = await get_info(1, 1, 0)
+    if json1["code"] != 0:
+        msg = "[CQ:at,qq={}]".format(id) + "\n接口获取数据异常，请联系管理员修复喵~"
+        await catch_str2.finish(Message(f'{msg}'))
+        return
+    try:
+        out_str = "\n昨日番剧如下：\n"
+        for i in range(len(json1["result"][0]["episodes"])):
+            out_str += "更新时间 " + json1["result"][0]["episodes"][i]["pub_time"] + " 【 " + \
+                       json1["result"][0]["episodes"][i]["title"] + " 】 " + \
+                       json1["result"][0]["episodes"][i]["pub_index"] + "\n"
+        msg = "[CQ:at,qq={}]".format(id) + out_str
+        await catch_str2.finish(Message(f'{msg}'))
+    except (KeyError, TypeError, IndexError) as e:
+        msg = "[CQ:at,qq={}]".format(id) + "\n接口解析异常，请联系管理员修复喵~"
+        await catch_str2.finish(Message(f'{msg}'))
+
+
+catch_str3 = on_keyword({'明日番剧'})
+
+@catch_str3.handle()
+async def send_msg2(bot: Bot, event: Event, state: T_State):
+    # get_msg = str(event.get_message())
+    # nonebot.logger.info(get_msg)
+    id = event.get_user_id()
+    json1 = await get_info(1, 0, 1)
+    if json1["code"] != 0:
+        msg = "[CQ:at,qq={}]".format(id) + "\n接口获取数据异常，请联系管理员修复喵~"
+        await catch_str3.finish(Message(f'{msg}'))
+        return
+    try:
+        out_str = "\n明日番剧如下：\n"
+        for i in range(len(json1["result"][1]["episodes"])):
+            out_str += "更新时间 " + json1["result"][1]["episodes"][i]["pub_time"] + " 【 " + \
+                       json1["result"][1]["episodes"][i]["title"] + " 】 " + \
+                       json1["result"][1]["episodes"][i]["pub_index"] + "\n"
+        msg = "[CQ:at,qq={}]".format(id) + out_str
+        await catch_str3.finish(Message(f'{msg}'))
+    except (KeyError, TypeError, IndexError) as e:
+        msg = "[CQ:at,qq={}]".format(id) + "\n接口解析异常，请联系管理员修复喵~"
+        await catch_str3.finish(Message(f'{msg}'))
+
+
+async def get_info(type=1, before=0, after=0):
     # 传参可以自行调整
-    API_URL = 'https://api.bilibili.com/pgc/web/timeline?types=1&before=0&after=0'
+    API_URL = 'https://api.bilibili.com/pgc/web/timeline?types=' + str(type) + '&before=' + str(before) + '&after=' + str(after)
     ret = requests.get(API_URL)
     ret = ret.json()
     # nonebot.logger.info(ret)
