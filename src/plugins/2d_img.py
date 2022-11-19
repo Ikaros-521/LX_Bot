561d73d3ec5595a16e4e3796e2b1f7b43ba7070a
Ikaros <327209194@qq.com>
2022-11-19 23:03:06 +0800

    合并随机返回二次元图片插件

--- src/plugins/2d_img.py
+++ src/plugins/2d_img.py
@@ -4,10 +4,9 @@ from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Bot, Event
 import nonebot
 import random
+import requests
 
 catch_str = on_keyword({'/二次元1'})
-
-
 @catch_str.handle()
 async def send_img(bot: Bot, event: Event, state: T_State):
     # get_msg = str(event.get_message())
@@ -15,3 +14,32 @@ async def send_img(bot: Bot, event: Event, state: T_State):
     # id = event.get_user_id()
     msg = "[CQ:image,file=https://api.vvhan.com/api/acgimg?" + str(random.random()) + "]"
     await catch_str.finish(Message(f'{msg}'))
+
+
+async def get_random_img():
+    API_URL = 'https://shengapi.cn/api/bizi.php?msg=2?' + str(random.random())
+    ret = requests.get(API_URL)
+    ret = ret.text
+    # nonebot.logger.info(ret)
+    url = ret[5:-1]
+    return url
+
+
+catch_str2 = on_keyword({'/二次元2'})
+@catch_str2.handle()
+async def send_img2(bot: Bot, event: Event, state: T_State):
+    # get_msg = str(event.get_message())
+    # nonebot.logger.info(get_msg)
+    # id = event.get_user_id()
+    msg = "[CQ:image,file=https://api.oick.cn/random/api.php?" + str(random.random()) + "]"
+    await catch_str2.finish(Message(f'{msg}'))
+
+
+catch_str3 = on_keyword({'/二次元4'})
+@catch_str3.handle()
+async def send_img3(bot: Bot, event: Event, state: T_State):
+    # get_msg = str(event.get_message())
+    # nonebot.logger.info(get_msg)
+    # id = event.get_user_id()
+    msg = "[CQ:image,file=https://iw233.cn/API/Random.php?" + str(random.random()) + "]"
+    await catch_str3.finish(Message(f'{msg}'))
