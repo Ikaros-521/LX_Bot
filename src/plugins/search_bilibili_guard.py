eb4817666a48b9484cef36a21d889bd7c15d02b1
Ikaros <327209194@qq.com>
2022-11-19 17:20:39 +0800

    优化 查舰团 查收益 命令功能（匹配关键词）；查收益 优化文字匹配类型

--- src/plugins/search_bilibili_guard.py
+++ src/plugins/search_bilibili_guard.py
@@ -14,8 +14,24 @@ from nonebot_plugin_htmlrender import (
     get_new_page,
 )
 
+# data文件有依赖性，如果单独装，请替换下
+from .nonebot_plugin_searchBiliInfo.data import DATA
+
 catch_str = on_keyword({'/查舰团 '})
 
+# 请求头贴入你的b站cookie
+header1 = {
+    'cookie': 'buvid3=52D30BB7-20FB-C7BA-60B7-EB3F18A8EB9C62437infoc; b_nut=1662632862; i-wanna-go-back=-1; b_ut=5; '
+              '_uuid=FB5921023-4BF10-D48E-10A62-EE619E5FA65E60948infoc; buvid_fp=3b2946a9aa52740147f7478c79476df7; '
+              'buvid4=0068E57C-9762-7898-4F73-0B8994558CF463776-022090818-FRmBv7s/ltnMkEnT97AnUQ%3D%3D; '
+              'fingerprint=eac7b07cd79cd55526b5a7206425ddc6; buvid_fp_plain=undefined; '
+              'SESSDATA=89e822a8%2C1678184893%2Cb9824%2A91; bili_jct=3d33d52161227b9ec082ba6124be8d63; '
+              'DedeUserID=1123180192; DedeUserID__ckMd5=6b55cc0fe28987d3; sid=8m93076x; PVID=25; '
+              'LIVE_BUVID=AUTO9316626329021634; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663557853,1663637087,'
+              '1663811728,1663935967; bp_video_offset_1123180192=707172485766840300; b_lsid=338EE34E_1836A5BC399; '
+              '_dfcaptcha=62ba3e19085f0e3de59bc4dcf7663756; innersign=0; '
+              'Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663937476 '
+}
 
 @catch_str.handle()
 async def send_img(bot: Bot, event: Event, state: T_State):
@@ -23,18 +39,32 @@ async def send_img(bot: Bot, event: Event, state: T_State):
     # nonebot.logger.info(get_msg)
     content = get_msg[5:]
 
-    # 数组中存放你想要快速匹配的用户，对应其uid填入uids数组
-    name = ['火羽', '猫雷', '莉爱', '雫酱', 'lulu', 'neol', 'koni', 'naru', '羽月']
-    uid = ['2094031249', '697091119', '1485277312', '1602464609', '387636363', '1300421811', '1372936974', '1354255177',
-           '1682889553']
-    try:
-        index = name.index(content)
-    except ValueError:
-        index = -1
-    # print(index)
+    # uid检索完成的标志位
+    flag = 0
 
-    if index != -1:
-        content = uid[index]
+    # 遍历本地DATA
+    for i in range(len(DATA)):
+        # 本地匹配到结果 就直接使用本地的(由于DATA源自https://api.vtbs.moe/v1/short，可能有空数据，需要异常处理下）
+        try:
+            if content == DATA[i]["uname"]:
+                content = str(DATA[i]["mid"])
+                flag = 1
+                break
+        except (KeyError, TypeError, IndexError) as e:
+            continue
+
+    # 本地没有匹配到，则从b站搜索
+    if flag != 1:
+        # 通过昵称查询uid，默认只查搜索到的第一个用户
+        info_json = await use_name_get_uid(content)
+        # nonebot.logger.info(info_json)
+
+        try:
+            result = info_json['data']['result']
+            # 只获取第一个搜索结果的数据
+            content = str(result[0]["mid"])
+        except (KeyError, TypeError, IndexError) as e:
+            nonebot.logger.info("查询不到用户名为：" + content + " 的相关信息")
 
     guard_info_json = await get_user_guard(content)
 
@@ -61,6 +91,16 @@ async def send_img(bot: Bot, event: Event, state: T_State):
 async def get_user_guard(uid):
     API_URL = 'https://api.tokyo.vtbs.moe/v1/guard/' + uid
     ret = requests.get(API_URL)
+    ret = ret.json()
+    # nonebot.logger.info(ret)
+    return ret
+
+
+# 通过昵称查询信息
+async def use_name_get_uid(name):
+    API_URL = 'https://api.bilibili.com/x/web-interface/search/type?page_size=10&keyword=' + name + \
+              '&search_type=bili_user'
+    ret = requests.get(API_URL, headers=header1)
     ret = ret.json()
     # nonebot.logger.info(ret)
     return ret
--- src/plugins/search_user_bili_live_income.py
+++ src/plugins/search_user_bili_live_income.py
@@ -14,8 +14,24 @@ from nonebot_plugin_htmlrender import (
     get_new_page,
 )
 
+# data文件有依赖性，如果单独装，请替换下
+from .nonebot_plugin_searchBiliInfo.data import DATA
+
 catch_str = on_keyword({'/查收益 '})
 
+# 请求头贴入你的b站cookie
+header1 = {
+    'cookie': 'buvid3=52D30BB7-20FB-C7BA-60B7-EB3F18A8EB9C62437infoc; b_nut=1662632862; i-wanna-go-back=-1; b_ut=5; '
+              '_uuid=FB5921023-4BF10-D48E-10A62-EE619E5FA65E60948infoc; buvid_fp=3b2946a9aa52740147f7478c79476df7; '
+              'buvid4=0068E57C-9762-7898-4F73-0B8994558CF463776-022090818-FRmBv7s/ltnMkEnT97AnUQ%3D%3D; '
+              'fingerprint=eac7b07cd79cd55526b5a7206425ddc6; buvid_fp_plain=undefined; '
+              'SESSDATA=89e822a8%2C1678184893%2Cb9824%2A91; bili_jct=3d33d52161227b9ec082ba6124be8d63; '
+              'DedeUserID=1123180192; DedeUserID__ckMd5=6b55cc0fe28987d3; sid=8m93076x; PVID=25; '
+              'LIVE_BUVID=AUTO9316626329021634; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663557853,1663637087,'
+              '1663811728,1663935967; bp_video_offset_1123180192=707172485766840300; b_lsid=338EE34E_1836A5BC399; '
+              '_dfcaptcha=62ba3e19085f0e3de59bc4dcf7663756; innersign=0; '
+              'Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663937476 '
+}
 
 @catch_str.handle()
 async def send_msg(bot: Bot, event: Event, state: T_State):
@@ -44,18 +60,32 @@ async def send_msg(bot: Bot, event: Event, state: T_State):
         await catch_str.finish(Message(f'{msg}'))
         return
 
-    # 数组中存放你想要快速匹配的用户，对应其uid填入uids数组
-    name = ['火羽', '猫雷', '莉爱', '雫酱', 'lulu', 'neol', 'koni', 'naru', '羽月']
-    uid = ['2094031249', '697091119', '1485277312', '1602464609', '387636363', '1300421811', '1372936974', '1354255177',
-           '1682889553']
-    try:
-        index = name.index(src_uid)
-    except ValueError:
-        index = -1
-    # print(index)
+    # uid检索完成的标志位
+    flag = 0
 
-    if index != -1:
-        src_uid = uid[index]
+    # 遍历本地DATA
+    for i in range(len(DATA)):
+        # 本地匹配到结果 就直接使用本地的(由于DATA源自https://api.vtbs.moe/v1/short，可能有空数据，需要异常处理下）
+        try:
+            if content == DATA[i]["uname"]:
+                src_uid = str(DATA[i]["mid"])
+                flag = 1
+                break
+        except (KeyError, TypeError, IndexError) as e:
+            continue
+
+    # 本地没有匹配到，则从b站搜索
+    if flag != 1:
+        # 通过昵称查询uid，默认只查搜索到的第一个用户
+        info_json = await use_name_get_uid(src_uid)
+        # nonebot.logger.info(info_json)
+
+        try:
+            result = info_json['data']['result']
+            # 只获取第一个搜索结果的数据
+            src_uid = str(result[0]["mid"])
+        except (KeyError, TypeError, IndexError) as e:
+            nonebot.logger.info("查询不到用户名为：" + src_uid + " 的相关信息")
 
     live_json = await get_live_info(src_uid)
 
@@ -73,7 +103,6 @@ async def send_msg(bot: Bot, event: Event, state: T_State):
         totalDanmakuCount = str(live_json["data"]["channel"]["totalDanmakuCount"])
         totalIncome = str(live_json["data"]["channel"]["totalIncome"])
         totalLivehour = str(round(live_json["data"]["channel"]["totalLiveSecond"] / 60 / 60, 2))
-
     except (KeyError, TypeError, IndexError) as e:
         msg = "[CQ:at,qq={}]".format(id) + '查询用户：' + src_uid + '失败,live_id解析失败,可能原因：场次数不对；无此场次'
         await catch_str.finish(Message(f'{msg}'))
@@ -84,6 +113,18 @@ async def send_msg(bot: Bot, event: Event, state: T_State):
               "  总直播时长:" + totalLivehour + "h\n\n" + \
               "| 时间 | uid | 昵称 | 内容 | 价格|\n" \
               "| :-----| :-----| :-----| :-----| :-----|\n"
+
+    # 默认1: 礼物，2: 上舰，3: SC
+    if income_type == "礼物":
+        income_type = "1"
+    elif income_type == "上舰":
+        income_type = "2"
+    elif income_type == "SC" | income_type == "sc" | income_type == "Sc":
+        income_type = "3"
+    else:
+        income_type = "1"
+
+    # nonebot.logger.info(out_str + "income_type:" + income_type)
 
     # 获取当场直播信息
     info_json = await get_info(live_id, income_type)
@@ -126,6 +167,16 @@ async def get_live_info(uid):
 async def get_info(live_id, income_type):
     API_URL = 'https://danmaku.suki.club/api/info/live?liveid=' + live_id + '&type=' + income_type + '&uid='
     ret = requests.get(API_URL, verify=False)
+    ret = ret.json()
+    # nonebot.logger.info(ret)
+    return ret
+
+
+# 通过昵称查询信息
+async def use_name_get_uid(name):
+    API_URL = 'https://api.bilibili.com/x/web-interface/search/type?page_size=10&keyword=' + name + \
+              '&search_type=bili_user'
+    ret = requests.get(API_URL, headers=header1)
     ret = ret.json()
     # nonebot.logger.info(ret)
     return ret
