1536136b335af2d12d7242c29f9ad02cffd4a26c
Ikaros <327209194@qq.com>
2022-11-19 16:58:45 +0800

    优化 查 命令功能

--- src/plugins/nonebot_plugin_searchBiliInfo/README.md
+++ src/plugins/nonebot_plugin_searchBiliInfo/README.md
@@ -1,18 +1,50 @@
+<div align="center">
+
 # nonebot_plugin_searchBiliInfo
+  
+_✨ NoneBot b站用户直播号、粉丝、舰团数查询插件 ✨_
+  
+
+<a href="./LICENSE">
+    <img src="https://img.shields.io/github/license/tkgs0/nonebot-plugin-antiinsult.svg" alt="license">
+</a>
+<a href="https://pypi.python.org/pypi/nonebot_plugin_searchBiliInfo">
+    <img src="https://img.shields.io/pypi/v/nonebot_plugin_searchBiliInfo.svg" alt="pypi">
+</a>
+<a href="https://www.python.org">
+    <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">
+</a>
+
+</div>
+
 适用于nonebot2 v11的b站用户粉丝、舰团信息查询插件（微调源码可以兼容其他版本）   
 调用的相关API均为b站官方接口  
 
-## 安装
+## 🔧 开发环境
+Nonebot2：2.0.0b5  
+python：3.8.13  
+操作系统：Windows10（CentOS7下正常运行，Linux兼容性问题不大）  
+编辑器：pycharm  
+
+## 💿 安装
 环境依赖requests库，记得安装下 `pip install requests`  
+### 本地安装
 将文件夹clone到你的机器人插件下的对应插件目录内（一般为机器人文件夹下的src/plugins）即可，也可以直接下载压缩包到插件目录解压。  
-目录结构： 你的bot/src/plugins/nonebot_plugin_searchBiliInfo  
+目录结构： ```你的bot/src/plugins/nonebot_plugin_searchBiliInfo```  
+```git clone https://github.com/Ikaros-521/nonebot_plugin_searchBiliInfo.git```  
+### pip安装
+```pip install nonebot_plugin_searchBiliInfo```  
+打开 nonebot2 项目的 ```bot.py``` 文件, 在其中写入  
+```nonebot.load_plugin('nonebot_plugin_antiinsult')```  
+当然，如果是默认配置的nonebot2的话，在bot路径```pyproject.toml```的```[tool.nonebot]```的```plugins```中添加```nonebot_plugin_searchBiliInfo```即可
 
-## 功能
-通过uid或设定好的短语查询指定b站用户的粉丝、舰团信息。
 
-## 命令
+## 🎉 功能
+通过uid或设定好的短语或b站接口搜索查询指定b站用户的粉丝、舰团信息。
+
+## 👉 命令
 命令结构：```/查 用户uid或设定好的昵称```  
-例如：```/查 1```  ```/查 lulu``` (昵称设定修改查看文档下方 拓展)  
+例如：```/查 用户uid 或 设定好的昵称 或 关键词```(昵称设定修改查看文档下方 拓展)
 bot返回内容：
 ```
 用户名：bishi
@@ -21,9 +53,44 @@ UID：1
 粉丝数：161841
 舰团数：0
 ```
-## 拓展
-命令修改：修改__init__.py的8行部分，catch_str变量的内容  
+## ⚙️ 拓展
+启用关键词搜索，需要修改__init__.py的header1的cookie，获取自己的cookie，填入。
 
-昵称设定：修改__init__.py的19,20行部分，name和uid这2个数组，按对应关系填入（索引要一一对应）  
+命令修改：修改data.py，在文件头部追加你需要定义的用户的json串，注意json格式！！！
 
-返回内容格式修改：修改__init__.py的42行部分，msg变量的内容  
+返回内容格式修改：修改__init__.py的75行部分，msg变量的内容  
+
+## 项目打包
+
+```
+参考 https://www.cnblogs.com/danhuai/p/14915042.html
+创建setup.py文件 填写相关信息
+
+# 4.1、可以先升级打包工具
+pip install --upgrade setuptools wheel twine
+
+# 4.2、打包
+python setup.py sdist bdist_wheel
+
+# 4.3、可以先检查一下包
+twine check dist/*
+
+# 4.4、上传包到pypi（需输入用户名、密码）
+twine upload dist/*
+```
+
+## 📝 更新日志
+
+<details>
+<summary>展开/收起</summary>
+
+### 0.0.1
+
+- 插件初次发布
+
+### 0.1.0
+
+- 更新基于vtbs.moe的主播数据，添加关键词搜索功能
+
+</details>
+
--- src/plugins/nonebot_plugin_searchBiliInfo/__init__.py
+++ src/plugins/nonebot_plugin_searchBiliInfo/__init__.py
@@ -5,8 +5,23 @@ from nonebot.adapters.onebot.v11 import Bot, Event
 import nonebot
 import requests
 
+from .data import DATA
+
 catch_str = on_keyword({'/查 '})
 
+# 请求头贴入你的b站cookie
+header1 = {
+    'cookie': 'buvid3=52D30BB7-20FB-C7BA-60B7-EB3F18A8EB9C62437infoc; b_nut=1662632862; i-wanna-go-back=-1; b_ut=5; '
+              '_uuid=FB5921023-4BF10-D48E-10A62-EE619E5FA65E60948infoc; buvid_fp=3b2946a9aa52740147f7478c79476df7; '
+              'buvid4=0068E57C-9762-7898-4F73-0B8994558CF463776-022090818-FRmBv7s/ltnMkEnT97AnUQ%3D%3D; '
+              'fingerprint=eac7b07cd79cd55526b5a7206425ddc6; buvid_fp_plain=undefined; '
+              'SESSDATA=89e822a8%2C1678184893%2Cb9824%2A91; bili_jct=3d33d52161227b9ec082ba6124be8d63; '
+              'DedeUserID=1123180192; DedeUserID__ckMd5=6b55cc0fe28987d3; sid=8m93076x; PVID=25; '
+              'LIVE_BUVID=AUTO9316626329021634; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663557853,1663637087,'
+              '1663811728,1663935967; bp_video_offset_1123180192=707172485766840300; b_lsid=338EE34E_1836A5BC399; '
+              '_dfcaptcha=62ba3e19085f0e3de59bc4dcf7663756; innersign=0; '
+              'Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663937476 '
+}
 
 @catch_str.handle()
 async def send_msg(bot: Bot, event: Event, state: T_State):
@@ -15,25 +30,43 @@ async def send_msg(bot: Bot, event: Event, state: T_State):
     # nonebot.logger.info(get_msg)
     content = get_msg[3:]
 
-    # 数组中存放你想要快速匹配的用户，对应其uid填入uids数组
-    name = ['火羽', '猫雷', '莉爱', '雫酱', 'lulu', 'neol', 'koni', 'naru', '羽月']
-    uid = ['2094031249', '697091119', '1485277312', '1602464609', '387636363', '1300421811', '1372936974', '1354255177', '1682889553']
-    try:
-        index = name.index(content)
-    except ValueError:
-        index = -1
-    # print(index)
+    # uid检索完成的标志位
+    flag = 0
 
-    if index != -1:
-        content = uid[index]
+    # 遍历本地DATA
+    for i in range(len(DATA)):
+        # 本地匹配到结果 就直接使用本地的(由于DATA源自https://api.vtbs.moe/v1/short，可能有空数据，需要异常处理下）
+        try:
+            if content == DATA[i]["uname"]:
+                content = str(DATA[i]["mid"])
+                flag = 1
+                break
+        except (KeyError, TypeError, IndexError) as e:
+            continue
 
+    # 本地没有匹配到，则从b站搜索
+    if flag != 1:
+        # 通过昵称查询uid，默认只查搜索到的第一个用户
+        info_json = await use_name_get_uid(content)
+        # nonebot.logger.info(info_json)
+
+        try:
+            result = info_json['data']['result']
+            # 只获取第一个搜索结果的数据
+            content = str(result[0]["mid"])
+        except (KeyError, TypeError, IndexError) as e:
+            nonebot.logger.info("查询不到用户名为：" + content + " 的相关信息")
+
+    # 传入uid获取用户基本信息
     base_info_json = await get_base_info(content)
     # 获取用户信息失败
     if base_info_json['code'] != 0:
         msg = "[CQ:at,qq={}]".format(id) + '\n获取uid：' + content + '，用户信息失败'
         await catch_str.finish(Message(f'{msg}'))
         return
+    # 获取用户直播间id
     room_id = await get_room_id(content)
+    # 没有直播间 默认为0
     if room_id == 0:
         guard_info_json = {"data": {"info": {"num": 0}}}
     else:
@@ -65,8 +98,19 @@ async def get_room_id(uid):
     return room_id
 
 
+# 获取舰团信息
 async def get_guard_info(uid, room_id):
     API_URL = 'https://api.live.bilibili.com/xlive/app-room/v2/guardTab/topList?roomid=' + str(room_id) + '&page=1&ruid=' + uid + '&page_size=0'
     ret = requests.get(API_URL)
     ret = ret.json()
+    return ret
+
+
+# 通过昵称查询信息
+async def use_name_get_uid(name):
+    API_URL = 'https://api.bilibili.com/x/web-interface/search/type?page_size=10&keyword=' + name + \
+              '&search_type=bili_user'
+    ret = requests.get(API_URL, headers=header1)
+    ret = ret.json()
+    # nonebot.logger.info(ret)
     return ret
