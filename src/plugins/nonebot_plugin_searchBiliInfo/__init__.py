a72786dcff6a8d9242dffb4d5df87a8657e25324
Ikaros <327209194@qq.com>
2022-11-21 00:42:34 +0800

    查直播 命令适应danmuku更新

--- src/plugins/nonebot_plugin_searchBiliInfo/__init__.py
+++ src/plugins/nonebot_plugin_searchBiliInfo/__init__.py
@@ -37,7 +37,16 @@ catch_str = on_keyword({'/查 '})
 
 # 请求头贴入你的b站cookie
 header1 = {
-    'cookie': ''
+    'cookie': 'buvid3=52D30BB7-20FB-C7BA-60B7-EB3F18A8EB9C62437infoc; b_nut=1662632862; i-wanna-go-back=-1; b_ut=5; '
+              '_uuid=FB5921023-4BF10-D48E-10A62-EE619E5FA65E60948infoc; buvid_fp=3b2946a9aa52740147f7478c79476df7; '
+              'buvid4=0068E57C-9762-7898-4F73-0B8994558CF463776-022090818-FRmBv7s/ltnMkEnT97AnUQ%3D%3D; '
+              'fingerprint=eac7b07cd79cd55526b5a7206425ddc6; buvid_fp_plain=undefined; '
+              'SESSDATA=89e822a8%2C1678184893%2Cb9824%2A91; bili_jct=3d33d52161227b9ec082ba6124be8d63; '
+              'DedeUserID=1123180192; DedeUserID__ckMd5=6b55cc0fe28987d3; sid=8m93076x; PVID=25; '
+              'LIVE_BUVID=AUTO9316626329021634; Hm_lvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663557853,1663637087,'
+              '1663811728,1663935967; bp_video_offset_1123180192=707172485766840300; b_lsid=338EE34E_1836A5BC399; '
+              '_dfcaptcha=62ba3e19085f0e3de59bc4dcf7663756; innersign=0; '
+              'Hm_lpvt_8a6e55dbd2870f0f5bc9194cddf32a02=1663937476 '
 }
 
 @catch_str.handle()
@@ -385,18 +394,27 @@ async def send_msg3(bot: Bot, event: Event, state: T_State):
         # 达到指定数量场次
         if i == int(info_size):
             break
-        if info_json["data"]["lives"][i]["stopDate"] is None:
+        try:
+            if info_json["data"]["lives"][i]["stopDate"] is None:
+                out_str += "| {:<s} | 直播中 | {:<s} | {:<d} | {:<d} | {:<d} | ￥{:<.1f} |".format(
+                    await timestamp_to_date(info_json["data"]["lives"][i]["startDate"]),
+                    info_json["data"]["lives"][i]["title"],
+                    info_json["data"]["lives"][i]["danmakusCount"],
+                    info_json["data"]["lives"][i]["watchCount"],
+                    info_json["data"]["lives"][i]["interactionCount"],
+                    info_json["data"]["lives"][i]["totalIncome"])
+            else:
+                out_str += "| {:<s} | {:<.2f}h | {:<s} | {:<d} | {:<d} | {:<d} | ￥{:<.1f} |".format(
+                    await timestamp_to_date(info_json["data"]["lives"][i]["startDate"]),
+                    (info_json["data"]["lives"][i]["stopDate"] - info_json["data"]["lives"][i]["startDate"]) / 1000 / 3600,
+                    info_json["data"]["lives"][i]["title"],
+                    info_json["data"]["lives"][i]["danmakusCount"],
+                    info_json["data"]["lives"][i]["watchCount"],
+                    info_json["data"]["lives"][i]["interactionCount"],
+                    info_json["data"]["lives"][i]["totalIncome"])
+        except (KeyError, TypeError, IndexError) as e:
             out_str += "| {:<s} | 直播中 | {:<s} | {:<d} | {:<d} | {:<d} | ￥{:<.1f} |".format(
                 await timestamp_to_date(info_json["data"]["lives"][i]["startDate"]),
-                info_json["data"]["lives"][i]["title"],
-                info_json["data"]["lives"][i]["danmakusCount"],
-                info_json["data"]["lives"][i]["watchCount"],
-                info_json["data"]["lives"][i]["interactionCount"],
-                info_json["data"]["lives"][i]["totalIncome"])
-        else:
-            out_str += "| {:<s} | {:<.2f}h | {:<s} | {:<d} | {:<d} | {:<d} | ￥{:<.1f} |".format(
-                await timestamp_to_date(info_json["data"]["lives"][i]["startDate"]),
-                (info_json["data"]["lives"][i]["stopDate"] - info_json["data"]["lives"][i]["startDate"]) / 1000 / 3600,
                 info_json["data"]["lives"][i]["title"],
                 info_json["data"]["lives"][i]["danmakusCount"],
                 info_json["data"]["lives"][i]["watchCount"],
