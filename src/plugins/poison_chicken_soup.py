eb9f47bd9d5cdb7e662d6eaa5e28d269892dd765
Ikaros <327209194@qq.com>
2022-12-08 10:25:40 +0800

    更换毒鸡汤接口

--- src/plugins/poison_chicken_soup.py
+++ src/plugins/poison_chicken_soup.py
@@ -15,15 +15,19 @@ async def send_msg(bot: Bot, event: Event, state: T_State):
     id = event.get_user_id()
 
     data = await get_data()
-    msg = '\n' + data['msg']
+    msg = '\n' + data['data']
 
     await catch_str.finish(Message(f'{msg}'), at_sender=True)
 
 
 async def get_data():
-    API_URL = 'https://api.linhun.vip/api/dujitang'
+    header1 = {
+        'content-type': 'text/plain; charset=utf-8',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 Core/1.94.186.400 QQBrowser/11.3.5195.400'
+    }
+    API_URL = 'https://www.iamwawa.cn/home/dujitang/ajax'
     async with aiohttp.ClientSession() as session:
-        async with session.get(url=API_URL) as response:
+        async with session.get(url=API_URL, headers=header1) as response:
             result = await response.read()
             ret = json.loads(result)
     # nonebot.logger.info(ret)
