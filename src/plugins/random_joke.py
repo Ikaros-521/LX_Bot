fdb2c29de7d5af02d8021bf0686a1e064a2aa241
Ikaros <327209194@qq.com>
2022-12-19 16:47:08 +0800

    新增随机笑话

--- src/plugins/random_joke.py
+++ src/plugins/random_joke.py
@@ -6,23 +6,24 @@ from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.params import CommandArg
 
 
-catch_str = on_command('星座')
+catch_str = on_command('笑话')
 
 
 @catch_str.handle()
 async def send_msg(bot: Bot, event: Event, msg: Message = CommandArg()):
-    content = msg.extract_plain_text()
-    data_json = await get_data(content)
-
-    msg = '\n' + json.dumps(data_json, indent=2, ensure_ascii=False)
+    data_json = await get_data()
+    try:
+        msg = '\n' + data_json["result"][0]["content"]
+    except:
+        msg = '数据解析失败，额度用完或接口寄了喵~'
 
     await catch_str.finish(Message(f'{msg}'), at_sender=True)
 
 
-async def get_data(content):
-    # 填写自己的api_key 获取地址：https://www.juhe.cn/docs/api/id/58
+async def get_data():
+    # 填写自己的api_key 获取地址：https://www.juhe.cn/docs/api/id/95
     api_key = ""
-    API_URL = 'http://web.juhe.cn/constellation/getAll?consName=' + content + '&type=today&key=' + api_key
+    API_URL = 'http://v.juhe.cn/joke/randJoke.php?key=' + api_key
     async with aiohttp.ClientSession() as session:
         async with session.get(url=API_URL) as response:
             result = await response.read()
