1c35c87b38ea1e4728148a468e7317e9964ab5ef
Ikaros <327209194@qq.com>
2023-01-06 15:57:30 +0800

    新增插件 听风客API，合并qq权重

---
+++ src/plugins/tfapi.py
@@ -0,0 +1,72 @@
+import aiohttp, json
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot import on_command
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.params import CommandArg
+
+
+# 这是一套on_command的模板 命令后的内容通通传入，调用的接口返回的json，转字符串后打印
+cmd1 = on_command('权重')
+@cmd1.handle()
+async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
+    content = msg.extract_plain_text()
+    API_URL = 'http://tfapi.top/API/qqqz.php?type=json&qq=' + content
+    data_json = await get_data_json(API_URL)
+
+    msg = '\n' + json.dumps(data_json, indent=2, ensure_ascii=False)
+
+    await cmd1.finish(Message(f'{msg}'), at_sender=True)
+
+
+# 这是一套on_command的模板 命令后的内容通通传入，调用的接口返回的字符串直接打印
+cmd2 = on_command('随机身份证')
+@cmd2.handle()
+async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
+    API_URL = 'http://tfapi.top/API/sjsfz.php'
+    data_text = await get_data_text(API_URL)
+
+    msg = '\n' + data_text
+
+    await cmd2.finish(Message(f'{msg}'), at_sender=True)
+
+
+cmd3 = on_command('随机网易小号')
+@cmd3.handle()
+async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
+    API_URL = 'http://tfapi.top/API/wyzh.php'
+    data_text = await get_data_text(API_URL)
+
+    msg = '\n' + data_text
+
+    await cmd3.finish(Message(f'{msg}'), at_sender=True)
+
+
+cmd4 = on_command('随机诱惑图')
+@cmd4.handle()
+async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
+    API_URL = 'http://tfapi.top/API/yht.php?return=txt'
+    data_text = await get_data_text(API_URL)
+
+    msg = '\n' + data_text
+
+    await cmd4.finish(Message(f'{msg}'), at_sender=True)
+
+
+# 传入拼接好的get请求链接 和 请求头(可以不传），返回json
+async def get_data_json(API_URL):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=API_URL) as response:
+            result = await response.read()
+            ret = json.loads(result)
+    # nonebot.logger.info(ret)
+    return ret
+
+
+# 传入拼接好的get请求链接 和 请求头(可以不传），返回字符串
+async def get_data_text(API_URL, header={}):
+    async with aiohttp.ClientSession(headers=header) as session:
+        async with session.get(url=API_URL, headers=header) as response:
+            ret = await response.read()
+    # nonebot.logger.info(ret)
+    return ret.decode('utf-8')
