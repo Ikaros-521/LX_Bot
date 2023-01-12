cb4ed6674afab0724970275aa8e5a15f33140784
Ikaros <327209194@qq.com>
2023-01-12 15:21:47 +0800

    修改z-lib插件返回结果为合并转发形式

--- src/plugins/z-lib.py
+++ src/plugins/z-lib.py
@@ -2,7 +2,7 @@ import aiohttp, json
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot import on_command
 from nonebot.typing import T_State
-from nonebot.adapters.onebot.v11 import Bot, Event
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, GroupMessageEvent
 from nonebot.params import CommandArg
 
 
@@ -10,7 +10,17 @@ catch_str = on_command('z-lib', aliases={"搜书"})
 
 
 @catch_str.handle()
-async def send_msg(bot: Bot, event: Event, msg: Message = CommandArg()):
+async def send_msg(bot: Bot, event: MessageEvent, msg: Message = CommandArg()):
+    # 信息源自 群聊或私聊
+    msg_from = "group"
+    if isinstance(event, GroupMessageEvent):
+        # nonebot.logger.info("群聊")
+        group = str(event.group_id)
+    else:
+        # nonebot.logger.info("私聊")
+        private = event.get_user_id()
+        msg_from = "private"
+
     content = msg.extract_plain_text()
 
     data = await get_data(content)
@@ -21,12 +31,31 @@ async def send_msg(bot: Bot, event: Event, msg: Message = CommandArg()):
 
     data_json = json.loads(data)
 
-    try:
-        msg = '\n' + json.dumps(data_json, indent=2, ensure_ascii=False) + '\n下载地址：https://gateway.pinata.cloud//ipfs/ 后面追加书的ipfs_cid即可。'
-    except:
-        msg = '数据解析失败，额度用完或接口寄了喵~'
+    msgList = []
+    msgList.extend(
+        [
+            MessageSegment.node_custom(
+                user_id=10000,
+                nickname="bot",
+                content=Message(MessageSegment.text(json.dumps(data_json, indent=2, ensure_ascii=False))),
+            ),
+            MessageSegment.node_custom(
+                user_id=10000,
+                nickname="bot",
+                content=Message(MessageSegment.text('下载地址：https://gateway.pinata.cloud//ipfs/ 链接后面追加书的ipfs_cid即可。')),
+            )
+        ]
+    ) 
 
-    await catch_str.finish(Message(f'{msg}'), at_sender=True)
+    try:
+        # 判断消息类型
+        if msg_from == "group":
+            await bot.send_group_forward_msg(group_id=group, messages=msgList)
+        else:
+            await bot.send_private_forward_msg(user_id=private, messages=msgList)
+    except:
+        msg = '果咩，数据发送失败喵~请查看源码和日志定位问题原因'
+        await catch_str.finish(Message(f'{msg}'), at_sender=True)
 
 
 async def get_data(content):
