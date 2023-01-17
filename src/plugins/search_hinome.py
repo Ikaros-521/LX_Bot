43542162272fb2bc4b58f69da9c682ac13271e5c
Ikaros <327209194@qq.com>
2023-01-17 14:07:02 +0800

    修改on_keyword为on_command，从而适配自定义的命令前缀

--- src/plugins/search_hinome.py
+++ src/plugins/search_hinome.py
@@ -21,7 +21,7 @@ header1 = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36 Core/1.94.186.400 QQBrowser/11.3.5195.400'
 }
 
-catch_str = on_keyword({'/查火羽'})
+catch_str = on_keyword({'/查火羽'}, priority=1)
 
 
 @catch_str.handle()
