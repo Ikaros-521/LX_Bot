49f303b47899655040ff48c299a32075f2585e0b
Ikaros <327209194@qq.com>
2023-02-22 09:47:09 +0800

    补充发病语录；完善发病语录文档，准备发版v0.0.1

--- src/plugins/nonebot_plugin_random_stereotypes/__init__.py
+++ src/plugins/nonebot_plugin_random_stereotypes/__init__.py
@@ -1,25 +1,21 @@
-from nonebot.adapters.onebot.v11 import Message, MessageSegment
-from nonebot import on_keyword
-from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Message
+from nonebot import on_command
+from nonebot.params import CommandArg
 from nonebot.adapters.onebot.v11 import Bot, Event
-import nonebot
+# import nonebot
 import random
 
 from .data import DATA
 
-catch_str = on_keyword({'/发病 '})
+catch_str = on_command("发病")
 
 
 @catch_str.handle()
-async def send_msg(bot: Bot, event: Event, state: T_State):
-    get_msg = str(event.get_message())
-    id = event.get_user_id()
-    # nonebot.logger.info(get_msg)
-    content = get_msg[4:]
+async def _(bot: Bot, event: Event, msg: Message = CommandArg()):
+    content = msg.extract_plain_text()
 
     random_num = random.randint(1, len(DATA)) - 1
 
-    msg = "[CQ:at,qq={}]".format(id) + '\n'
-    msg += DATA[random_num].format(target_name=content)
+    msg = DATA[random_num].format(target_name=content)
 
-    await catch_str.finish(Message(f'{msg}'))
+    await catch_str.finish(Message(f'{msg}'), at_sender=True)
