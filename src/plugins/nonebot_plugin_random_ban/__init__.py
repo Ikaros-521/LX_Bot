65fb030095ea53eaca24822b7f5fdd12aeef06b5
Ikaros <327209194@qq.com>
2023-06-08 18:52:49 +0800

    随机禁言插件升级v0.2.0

--- src/plugins/nonebot_plugin_random_ban/__init__.py
+++ src/plugins/nonebot_plugin_random_ban/__init__.py
@@ -4,18 +4,19 @@ from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent, GROUP_ADM
 from nonebot.permission import SUPERUSER
 from nonebot.params import CommandArg
 import nonebot
-import random
+import random, re
 from nonebot.plugin import PluginMetadata
+from nonebot.exception import FinishedException
 
 
 help_text = f"""
 随机禁言一名群员
 命令结构：/随机禁言 [最大禁言时间] 或 /随禁 [最大禁言时间] （最大禁言时间不填默认60分钟内的随机）  
-例如：/随机禁言 或 /随禁 10  
+例如：/随机禁言 或 /随禁 10 或 /随禁 10分 或 /随禁 10时 或 /随禁 10天 
 
 随机禁言自己
 命令结构：/口球 [最大禁言时间] 或 /禁我 [最大禁言时间] （最大禁言时间不填默认60分钟内的随机）  
-例如：/口球 或 /禁我 10  
+例如：/口球 或 /禁我 10 或 /禁我 10分 或 /口球 10时 或 /口球 10天 
 
 开启至暗时刻
 命令结构：```/开启至暗时刻``` 或 ```/至暗时刻启动``` 或 ```/至暗时刻开启```  或 ```/启动至暗时刻```  
@@ -47,9 +48,58 @@ except:
 catch_str = on_command('随机禁言', aliases={"随禁"})
 
 
+# 正则匹配时间
+def parse_time_string(time_string):
+    pattern = r'(\d+(?:\.\d+)?)(?:分钟|分|小时|时|天)?'
+    match = re.match(pattern, time_string)
+
+    # print(f"time_string={time_string}")
+
+    if match:
+        groups = match.groups()
+        num_groups = len(groups)
+
+        if num_groups == 1:
+            value = float(groups[0])
+            return int(value)  # 纯数字情况
+
+        value = float(match.group(1))
+        unit = match.group(2)
+
+        if unit == '分钟' or unit == '分':
+            return int(value)
+        elif unit == '小时' or unit == '时':
+            return int(value * 60)
+        elif unit == '天':
+            return int(value * 60 * 24)
+
+        # 纯数字情况
+        return int(value)
+
+    return None
+
+
+# 分钟数转换为 "x天x小时x分钟"
+def format_minutes(minutes):
+    days = minutes // (60 * 24)
+    hours = (minutes % (60 * 24)) // 60
+    remaining_minutes = minutes % 60
+
+    result = ""
+    if days > 0:
+        result += str(days) + "天"
+    if hours > 0:
+        result += str(hours) + "小时"
+    if remaining_minutes > 0:
+        result += str(remaining_minutes) + "分钟"
+
+    return result
+
+
+
 @catch_str.handle()
 async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg()):
-    content = msg.extract_plain_text()
+    content = msg.extract_plain_text().strip()
     # nonebot.logger.info(content)
 
     group_id = event.group_id
@@ -64,10 +114,17 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
             max_ban_time = random.randint(1, 60)
             try:
                 if len(content) > 0:
-                    max_ban_time = random.randint(1, int(content))
-            except:
-                msg = "\n参数解析失败（请传入正整数的最大禁言时间）"
-                await catch_str.finish(Message(f'{msg}'), at_sender=True)
+                    tmp = parse_time_string(content)
+                    if tmp is None:
+                        msg = "参数解析失败（请传入正整数的最大禁言时间）"
+                        await catch_str.finish(Message(f'{msg}'), reply_message=True)
+                    max_ban_time = random.randint(1, tmp)
+            except FinishedException:
+                pass
+            except Exception as e:
+                print(e)
+                msg = "参数解析失败（请传入正整数的最大禁言时间）"
+                await catch_str.finish(Message(f'{msg}'), reply_message=True)
             # 获取群号 event.group_id
             member_list = await bot.get_group_member_list(group_id=group_id)
 
@@ -78,25 +135,36 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
             # nonebot.logger.info("nickname:" + nickname + " user_id=" + str(user_id) + " time=" + str(max_ban_time))
             try:
                 await bot.set_group_ban(group_id=group_id, user_id=user_id, duration=60 * max_ban_time)
-            except:
-                msg = "\n禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
-                await catch_str.finish(Message(f'{msg}'), at_sender=True)
+            except FinishedException:
+                pass
+            except Exception as e:
+                print(e)
+                msg = "禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
+                await catch_str.finish(Message(f'{msg}'), reply_message=True)
 
-            msg = "\n恭喜幸运儿:" + nickname + " 获得" + str(max_ban_time) + "分钟的禁言服务"
-            await catch_str.finish(Message(f'{msg}'), at_sender=True)
+            msg = "恭喜幸运儿:" + nickname + " 获得" + format_minutes(max_ban_time) + "的禁言服务"
+            await catch_str.finish(Message(f'{msg}'), reply_message=True)
         else:
-            msg = '\n本群的至暗时刻还没开启，请告知群管发送"/至暗时刻开启"，启用功能'
-            await catch_str.finish(Message(f'{msg}'), at_sender=True)
+            msg = '本群的至暗时刻还没开启，请告知群管发送"/至暗时刻开启"，启用功能'
+            await catch_str.finish(Message(f'{msg}'), reply_message=True)
     # 是群管的话 随便用
     else:
         # 最大禁言时间
         max_ban_time = random.randint(1, 60)
         try:
             if len(content) > 0:
-                max_ban_time = random.randint(1, int(content))
-        except:
-            msg = "\n参数解析失败（请传入正整数的最大禁言时间）"
-            await catch_str.finish(Message(f'{msg}'), at_sender=True)
+                tmp = parse_time_string(content)
+                # print(f"tmp={tmp}")
+                if tmp is None:
+                    msg = "参数解析失败（请传入正整数的最大禁言时间）"
+                    await catch_str.finish(Message(f'{msg}'), reply_message=True)
+                max_ban_time = random.randint(1, tmp)
+        except FinishedException:
+            pass
+        except Exception as e:
+            print(e)
+            msg = "参数解析失败（请传入正整数的最大禁言时间）"
+            await catch_str.finish(Message(f'{msg}'), reply_message=True)
         # 获取群号 event.group_id
         member_list = await bot.get_group_member_list(group_id=group_id)
 
@@ -107,12 +175,15 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
         # nonebot.logger.info("nickname:" + nickname + " user_id=" + str(user_id) + " time=" + str(max_ban_time))
         try:
             await bot.set_group_ban(group_id=group_id, user_id=user_id, duration=60 * max_ban_time)
-        except:
-            msg = "\n禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
-            await catch_str.finish(Message(f'{msg}'), at_sender=True)
+        except FinishedException:
+            pass
+        except Exception as e:
+            print(e)
+            msg = "禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
+            await catch_str.finish(Message(f'{msg}'), reply_message=True)
 
-        msg = "\n恭喜幸运儿:" + nickname + " 获得" + str(max_ban_time) + "分钟的禁言服务"
-        await catch_str.finish(Message(f'{msg}'), at_sender=True)
+        msg = "恭喜幸运儿:" + nickname + " 获得" + format_minutes(max_ban_time) + "的禁言服务"
+        await catch_str.finish(Message(f'{msg}'), reply_message=True)
 
 
 catch_str2 = on_command('口球', aliases={"禁我"})
@@ -121,16 +192,23 @@ catch_str2 = on_command('口球', aliases={"禁我"})
 @catch_str2.handle()
 async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg()):
     user_id = event.get_user_id()
-    content = msg.extract_plain_text()
+    content = msg.extract_plain_text().strip()
     # nonebot.logger.info(content)
     # 最大禁言时间
     max_ban_time = random.randint(1, 60)
     try:
         if len(content) > 0:
-            max_ban_time = random.randint(1, int(content))
-    except:
-        msg = "\n参数解析失败（请传入正整数的最大禁言时间）"
-        await catch_str2.finish(Message(f'{msg}'), at_sender=True)
+            tmp = parse_time_string(content)
+            if tmp is None:
+                msg = "参数解析失败（请传入正整数的最大禁言时间）"
+                await catch_str.finish(Message(f'{msg}'), reply_message=True)
+            max_ban_time = random.randint(1, tmp)
+    except FinishedException:
+        pass
+    except Exception as e:
+        print(e)
+        msg = "参数解析失败（请传入正整数的最大禁言时间）"
+        await catch_str2.finish(Message(f'{msg}'), reply_message=True)
     # 获取群号 event.group_id
     # member_list = await bot.get_group_member_list(group_id=event.group_id)
 
@@ -139,12 +217,13 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
     # nonebot.logger.info("nickname:" + nickname + " user_id=" + str(user_id) + " time=" + str(max_ban_time))
     try:
         await bot.set_group_ban(group_id=event.group_id, user_id=user_id, duration=60 * max_ban_time)
-    except:
-        msg = "\n禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
-        await catch_str2.finish(Message(f'{msg}'), at_sender=True)
+    except Exception as e:
+        print(e)
+        msg = "禁言失败~机器人权限不足（请确认bot是否是管理员/禁言到群管）"
+        await catch_str2.finish(Message(f'{msg}'), reply_message=True)
 
-    msg = "\n恭喜您获得" + str(max_ban_time) + "分钟的禁言服务"
-    await catch_str2.finish(Message(f'{msg}'), at_sender=True)
+    msg = "恭喜您获得" + format_minutes(max_ban_time) + "的禁言服务"
+    await catch_str2.finish(Message(f'{msg}'), reply_message=True)
 
 
 catch_str3 = on_command('至暗时刻启动', aliases={"开启至暗时刻", "至暗时刻开启", "启动至暗时刻"}, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER)
@@ -160,12 +239,12 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
 
     # 如果存在此群配置
     if group_id in anyone_can_random_ban:
-        msg = "\n本群已开启 至暗时刻，无需重复启动。"
-        await catch_str3.finish(Message(f'{msg}'), at_sender=True)
+        msg = "本群已开启 至暗时刻，无需重复启动。"
+        await catch_str3.finish(Message(f'{msg}'), reply_message=True)
     else:
         anyone_can_random_ban.append(group_id)
-        msg = "\n本群开启 至暗时刻成功，开始狩猎吧！"
-        await catch_str3.finish(Message(f'{msg}'), at_sender=True)
+        msg = "本群开启 至暗时刻成功，开始狩猎吧！"
+        await catch_str3.finish(Message(f'{msg}'), reply_message=True)
 
 
 catch_str4 = on_command('至暗时刻关闭', aliases={"关闭至暗时刻", "停止至暗时刻", "至暗时刻停止"}, permission=SUPERUSER | GROUP_ADMIN | GROUP_OWNER)
@@ -182,9 +261,8 @@ async def send_msg(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg
     # 如果存在此群配置
     if group_id in anyone_can_random_ban:
         anyone_can_random_ban.remove(group_id)
-        msg = "\n本群已关闭 至暗时刻，世界恢复和平。"
-        await catch_str4.finish(Message(f'{msg}'), at_sender=True)
+        msg = "本群已关闭 至暗时刻，世界恢复和平。"
+        await catch_str4.finish(Message(f'{msg}'), reply_message=True)
     else:
-        msg = "\n本群未开启 至暗时刻，无需关闭。"
-        await catch_str4.finish(Message(f'{msg}'), at_sender=True)
-
+        msg = "本群未开启 至暗时刻，无需关闭。"
+        await catch_str4.finish(Message(f'{msg}'), reply_message=True)
